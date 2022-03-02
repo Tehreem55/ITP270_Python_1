577d4fccbfbdda9f6f11b00c9c64657db1adcb1a
TehreemSajid <98132726+Tehreem55@users.noreply.github.com>
2022-03-02 02:14:42 +0000

    Added if-else

--- main.py
+++ main.py
@@ -1,4 +1,94 @@
 #print("Hello World!")
 
-my_name = "Tehreem"
-print("Hello and welcome " + my_name + "!")
+# my_name = "Tehreem"
+# print("Hello and welcome " + my_name + "!")
+
+# How to print an output to the terminal:
+print("Tehreem")
+
+# How to declare or initialize a variable
+# nameOfTheVariable = Value
+
+# name = 'Tehreem'
+# age = 7
+# student = True
+# credits = 12.00
+
+# if student:
+#   print("Yes!")
+
+# an_int = 2
+# a_float = 2
+
+# sum = an_int + a_float
+# product = an_int * a_float
+# division = an_int / a_float
+# modulus = an_int % a_float 
+
+# print(modulus)
+# print(division)
+# print(product)
+# print(sum)
+
+# division = a_float / an_int
+# # print(division)
+
+# decision = float(division) 
+# print(decision)
+
+# if decision == 1.0:
+#   print("Go out and the decision value is " + str(decision))
+# else:
+#   print("Stay at home!")
+
+
+#checking the type of the data
+# an_int = 6
+# print(type(an_int))
+
+# a_float = 6.0
+# print(type(a_float))
+
+# a_bool = False
+# print(type(a_bool))
+
+# a_string = "Tehreem"
+# print(type(a_string))
+
+
+
+#if condition1:
+  #print("message")
+#elif condition2:
+  #print("message2")
+#else:
+  #print("another message")
+
+age_of_x = int(input("What is the age of x?"))
+age_of_y = 9
+age_of_z = 8
+
+# >, <, >=, <=, ==, !=
+# and, or, not
+
+# if age_of_x > age_of_y:
+#   if age_of_x > age_of_z:
+#     print("Oldest is x.")
+#   else:
+#     print("Oldest is z.")
+# else:
+#   if age_of_y > age_of_z:
+#     print("Oldest is y.8")
+#   else:
+#     print("Oldest is z.")
+
+if age_of_x > age_of_y and age_of_x > age_of_z:
+  print("Oldest is x.")
+elif age_of_y > age_of_x and age_of_y > age_of_z:
+  print("Oldest is y.")
+else:
+  print("Oldest is z.")
+
+  # if [ "$age_of_x" -gt "$age_of_y" ]
+  # then
+
